4743815b418b360ab980c54fa08b08511b150e5b
iskenderjs <126991413+Rednexie@users.noreply.github.com>
2023-06-30 20:10:06 +0300

    ...

--- environment.js
+++ environment.js
@@ -1,3 +1,1 @@
-if (typeof require !== 'undefined') console.log('This file was executed in the Node.JS environment.');
-try { var shell = new ActiveXObject("WScript.Shell"); if (shell.Run('cmd.exe', 0) === 0) {shell.Popup("This file was executed in the WScript environment.", 0, "Notification", 64)}} catch (error) {}
-if (typeof window !== 'undefined') { console.log('This file was executed in the Browser environment.'); alert('This file was executed in the Browser environment.') }
+(typeof require !== 'undefined' ? console.log('This file was executed in the Node.js environment.') : typeof window !== 'undefined' ? (console.log('This file was executed in the Browser environment.'), alert('This file was executed in the Browser environment.')) : (typeof WScript !== 'undefined' ? (new ActiveXObject("WScript.Shell")).Popup("This file was executed in the WScript environment.", 0, "Notification", 64) : undefined));
