865d04b7fee46edfd93a17c74a8fa864155f4190
Rednexie <126991413+Rednexie@users.noreply.github.com>
2023-11-06 11:42:40 +0300

    Update environment.js

--- environment.js
+++ environment.js
@@ -1,8 +1,8 @@
 let env = 0;
 
-(typeof require !== 'undefined' ? env = 2 : typeof window !== 'undefined' ? (env = 1, alert('This file was executed in the Browser environment.')) : (typeof WScript !== 'undefined' ? (env = 3; new ActiveXObject("WScript.Shell")).Popup("This file was executed in the WScript environment.", 0, "Notification", 64) : undefined));
+(typeof require == 'function' ? env = 2 : typeof window !== 'undefined' ? (env = 1, alert('This file was executed in the Browser environment.')) : (typeof WScript !== 'undefined' ? (env = 3; new ActiveXObject("WScript.Shell")).Popup("This file was executed in the WScript environment.", 0, "Notification", 64) : undefined));
 
-
+// 0 = unknown
 // 1 = browser
 // 2 = node
 // 3 = wscript
